20de8dbd1b6121d346d6a9679d2e7b237f6cdc16
ata_tend <145768629+alinatenduk@users.noreply.github.com>
2024-01-05 14:51:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const hb = document.querySelector('.header_burger_btn');
+const menu = document.querySelector('.header');
+
+hb.addEventListener('click', function(){
+    menu.classList.toggle('active');
+})
+
+//menu
+document.addEventListener('DOMContentLoaded', function () {
+    // Get links from your menu
+    var mentorsLink = document.querySelector('a[href="#mentors"]');
+
+    // Add a click handler for scrolling and centering above
+    mentorsLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        //Get a block of mentors by its id
+        var mentorsBlock = document.getElementById('mentors');
+        var windowHeight = window.innerHeight;
+        mentorsBlock.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        window.scrollBy(0, 700);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerBtn = document.querySelector('.header_burger_btn');
+    const menu = document.querySelector('.menu ul');
+
+    burgerBtn.addEventListener('click', function () {
+        menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
+    });
+});
+
+
+function showFeatures() {
+    alert("Ви переглядаєте можливості!");
+}
+
+function downloadNow() {
+    alert("Завантаження розпочалося!");
+}
+
+
+
+
+
+
